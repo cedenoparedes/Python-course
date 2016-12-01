9fb093af7eaf77532879c7d44b997192d9f27725
Eber <cedenoparedes@gmail.com>
2016-11-30 21:37:30 -0400

    numeros enteros

---
+++ numeros enteros reales y operadores aritmetico.py
@@ -0,0 +1,32 @@
+real = 0.567
+real = 0.56e3
+
+print real
+
+a = 26
+b = 11.3
+c = 5
+d = 3.5
+
+#suma
+print a + b
+#resta
+print c - a
+#multiplicacion
+print d * c
+#exponente
+print c ** 2
+#divicion
+print float (c) / a
+#divicion entera
+print 7 / 3
+#modulo
+print 7%3
+
+
+
+
+
+
+
+
