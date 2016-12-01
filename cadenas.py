38ce87ad0818ea986ae09a7bad4862fcf324bd44
Eber <cedenoparedes@gmail.com>
2016-11-30 22:00:42 -0400

    test cadenas

---
+++ cadenas.py
@@ -0,0 +1,29 @@
+#cadenas
+cadn = 'hola soy Eber'
+cdns = "hola soy Eber de nuevo"
+
+print type (cadn)
+print cadn
+print cdns
+#caracter de escape
+cdns = "hola soy \n Eber de nuevo"
+
+print cdns
+
+#salto de linea y tab
+cdns = "hola soy \n\t Eber de nuevo"
+
+print cdns
+
+#triple comilla """ crea los saltos de lineas literales
+
+saludo = """ Eber
+Cedeno
+hola
+123
+       1
+fin
+"""
+
+print saludo
+
