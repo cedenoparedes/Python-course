2772a104dd004d2be98cc63da78602fc9c10f93b
Eber <cedenoparedes@gmail.com>
2016-12-01 11:38:28 -0400

    concatenacion y reoeticion de cadena

---
+++ Repeticion y concatenacion.py
@@ -0,0 +1,27 @@
+#repeticion de cadenas
+
+nombre =  "eber" * 5
+apellido = "cedeno"
+
+print nombre
+
+#concatenar
+
+print nombre + apellido
+
+#Valores Booleanos
+
+bt = True
+bf = False
+
+#Operadores logicos
+
+beand = True and False
+bor = True or False
+bnot = not True
+
+print beand
+print bor
+print bnot
+
+
